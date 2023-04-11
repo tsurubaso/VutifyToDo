0ea6cef907e1ec93d1045857e39eed8b78082b21
tsurubaso <tsurubaso@gmail.com>
2023-04-11 17:37:45 +0900

    work on vuetify

---
+++ portfolio/src/layouts/default/AppBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-toolbar
+    color="light-green"
+      dark
+      prominent
+      
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title >My super shop
+        <v-icon  class="mdi mdi-account-cowboy-hat-outline"></v-icon>
+      </v-toolbar-title>
+      
+
+      <v-spacer></v-spacer>
+
+     <v-toolbar-items>
+      <v-btn>About</v-btn>
+      <v-btn>Contact</v-btn>
+      <v-btn>Login</v-btn>
+     </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
+
+<script setup>
+  //
+</script>
